96485d661a7ad5c5f7872db86fdf206d05036583
wangkexue <wang.mingwan@gmail.com>
2023-04-02 18:33:04 -0700

    GPT 4 1st attempt runs

--- amazon_stock_chart.py
+++ amazon_stock_chart.py
@@ -2,40 +2,30 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Define the ticker symbol for Amazon
-symbol = "AMZN"
+# Download Amazon's stock data
+ticker = "AMZN"
+start_date = "2020-01-01"
+end_date = "2023-01-01"
 
-# Define the start and end dates for the data
-start_date = "2015-01-01"
-end_date = "2021-12-31"
+data = yf.download(ticker, start=start_date, end=end_date)
 
-# Retrieve the historical data for the stock price of Amazon
-data = yf.download(symbol, start=start_date, end=end_date)
+# Calculate the PS bands
+window = 20
+data['Upper_PS'] = data['High'].rolling(window=window).max()
+data['Lower_PS'] = data['Low'].rolling(window=window).min()
 
-# Read the market capitalization and revenue data from a CSV file
-# You can download the CSV file from the SEC website or other financial data providers
-financials = pd.read_csv("https://www.sec.gov/files/company_tickers/ticker_master.csv")
-market_cap = financials[financials["ticker"] == symbol]["market_cap"].values[0]
-revenue = financials[financials["ticker"] == symbol]["revenue"].values[0]
+# Create a plot
+plt.figure(figsize=(14, 8))
+plt.plot(data.index, data['Close'], label=f'{ticker} Close Price', color='black', alpha=0.5)
+plt.plot(data.index, data['Upper_PS'], label='Upper PS Band', color='red', alpha=0.7)
+plt.plot(data.index, data['Lower_PS'], label='Lower PS Band', color='blue', alpha=0.7)
 
-# Calculate the PS ratio
-ps_ratio = market_cap / revenue
+# Customize the plot
+plt.title(f'{ticker} Stock Chart with PS Bands')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.legend(loc='upper left')
+plt.grid()
 
-# Plot the stock price and PS ratio as subplots
-fig, axs = plt.subplots(2, sharex=True, figsize=(12, 8))
-fig.suptitle("Amazon Stock Price and Price-to-Sales Ratio (2015-2021)")
-
-# Plot the stock price as the first subplot
-axs[0].plot(data["Close"])
-axs[0].set_ylabel("Stock Price ($)")
-axs[0].grid(True)
-
-# Plot the PS ratio as the second subplot
-axs[1].fill_between(data.index, ps_ratio, alpha=0.3)
-axs[1].plot(ps_ratio, color="blue")
-axs[1].set_ylabel("Price-to-Sales Ratio")
-axs[1].grid(True)
-
-# Save the chart as a PNG file
-plt.savefig("amazon_stock_price_ps_ratio.png")
+# Show the plot
 plt.show()
